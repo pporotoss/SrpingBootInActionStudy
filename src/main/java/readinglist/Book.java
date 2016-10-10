07a3f00d9c23d2c5a542b0b645479f219ab6418a
KWON <pporotoss@gmail.com>
2016-10-10 20:13:50 +0900

    Ch2 시작.

---
+++ src/main/java/readinglist/Book.java
@@ -0,0 +1,20 @@
+package readinglist;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String reader;
+	private String isbn;
+	private String title;
+	private String author;
+	private String description;
+	
+}
